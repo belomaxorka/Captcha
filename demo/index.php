8738ca8614aa5802cba774464e0de52bc6038f65
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-03-09 08:20:31 +0300

    Added links to other demos in index.php

--- demo/index.php
+++ demo/index.php
@@ -6,6 +6,16 @@
 </head>
 <body>
 <div>
+    <h1>Other examples:</h1>
+    <ul>
+        <li><a href="demo.php">Demo (Saves out.jpg local)</a></li>
+        <li><a href="inline.php">Inline</a></li>
+        <li><a href="form.php">Form</a></li>
+        <li><a href="output.php">Output</a></li>
+        <li><a href="fingerprint.php">Fingerprint</a></li>
+        <li><a href="ocr.php">Ocrad</a></li>
+        <li><a href="session.php">Session</a></li>
+    </ul>
     <h1>Captcha gallery</h1>
     <?php for ($x = 0; $x < 8; $x++) { ?>
         <?php for ($y = 0; $y < 5; $y++) { ?>
