7eea922ba25f4c0f58deedb745ec80ebf036abbb
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-03-10 15:32:38 +0300

    Added notice in README for `setScatterEffect` method

--- src/Gregwar/Captcha/ImageFileHandler.php
+++ src/Gregwar/Captcha/ImageFileHandler.php
@@ -53,7 +53,7 @@ class ImageFileHandler
     /**
      * Saves the provided image content as a file
      *
-     * @param string $contents
+     * @param $contents
      *
      * @return string
      */
